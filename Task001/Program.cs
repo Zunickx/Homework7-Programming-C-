3eae4d62426ba3691995d34b3aaa263983c19794
Zunickx <Zunickx@gmail.com>
2022-08-29 22:57:41 +0300

    Task001 Заполненный массив вещественных чисел

---
+++ Task001/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+
+Console.WriteLine("Введите кол-во m-строк массива: ");
+string m = Console.ReadLine()!;
+Console.WriteLine("Введите кол-во n-столбцов массива: ");
+string n = Console.ReadLine()!;
+
+void ArrayFilling()
+{
+    double[,] matrix = new double[int.Parse(m), int.Parse(n)];
+    Random matrixX = new Random();
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = matrixX.Next(-1000, 1000);
+            Console.Write($"{(matrix[i, j]/10)} ");
+        }
+        Console.WriteLine();
+    }
+    
+}
+ArrayFilling();
